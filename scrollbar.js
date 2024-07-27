4b5e937e9cd5e0c0bc6b2d3e85ebce2f91a4b6eb
Walburr <145846387+Walburr@users.noreply.github.com>
2024-07-27 11:40:56 +0200

    jsp
    i try to make a btn for go to the bottom and the top in only one or with the less code ligne

--- scrollbar.js
+++ scrollbar.js
@@ -4,29 +4,45 @@ document.addEventListener("DOMContentLoaded", function() {
         var documentHeight = document.documentElement.scrollHeight - window.innerHeight;
 
         var zeroFraction = 0.1; 
+        var zdotfFraction = 0.05; 
         var firstFraction = 0.2; 
         var secondFraction = 0.4; 
+        var secdotthFraction = 0.5; 
         var thirdFraction = 0.6; 
         var fourthFraction = 0.8; 
         
         var zerov = documentHeight * zeroFraction;
+        var zdotfv = documentHeight * zdotfFraction;
         var firstv = documentHeight * firstFraction;
         var secondv = documentHeight * secondFraction;
+        var secdotthv = documentHeight * secdotthFraction;
         var thirdv = documentHeight * thirdFraction;
         var fourthv = documentHeight * fourthFraction;
 
-        document.documentElement.classList.remove('scrollbar-first', 'scrollbar-second', 'scrollbar-third', 'scrollbar-fourth');
+
+        document.documentElement.classList.remove(
+            'scrollbar-zero', 'scrollbar-zdotf', 'scrollbar-first',
+            'scrollbar-second', 'scrollbar-sedotth', 'scrollbar-third',
+            'scrollbar-fourth'
+        );
 
         if (scrollTop >= fourthv) {
+            console.log("pipi")
             document.documentElement.classList.add('scrollbar-fourth');
         } else if (scrollTop >= thirdv) {
+            console.log("caca")
             document.documentElement.classList.add('scrollbar-third');
+        } else if (scrollTop >= secdotthv) {
+            document.documentElement.classList.add('scrollbar-sedotth');
         } else if (scrollTop >= secondv) {
             document.documentElement.classList.add('scrollbar-second');
         } else if (scrollTop >= firstv) {
             document.documentElement.classList.add('scrollbar-first');
-        }else if (scrollTop >= zerov) {
+        } else if (scrollTop >= zerov) {
             document.documentElement.classList.add('scrollbar-zero');
+        } else if (scrollTop >= zdotfv) {
+            document.documentElement.classList.add('scrollbar-zdotf');
         }
     });
 });
+
--- wanabur.js
+++ wanabur.js
@@ -539,28 +539,7 @@ function DMOD() {
     window.location.href = "../../mod.html"
 }
 
-// // Fonction pour détecter le défilement
-// function handleScroll() {
-//     var text = document.getElementById('text_bur');
-//     var ref_top = document.getElementById('p-spinner');
-//     var triggerPositionTop = ref_top.getBoundingClientRect().top;
-//     var windowHeight = window.innerHeight;
-  
-//     // Si le haut ou le bas du déclencheur est visible
-//     if (triggerPosition < windowHeight) {
-//       // Ajoutez une classe pour activer l'animation
-//       document.querySelector('.typing-animation').classList.add('active');
-//       text.style.display = 'flex'
-//     } else {
-//       // Sinon, enlever la classe pour désactiver l'animation
-//       document.querySelector('.typing-animation').classList.remove('active');
-//       text.style.display = 'none';
-//     }
-//   }
-  
-//   // Ajouter un écouteur d'événements pour le défilement
-//   window.addEventListener('scroll', handleScroll);
-// Fonction pour détecter le défilement
+
 const title = document.querySelector('.title')
 const leaf1 = document.querySelector('.leaf1')
 const leaf2 = document.querySelector('.leaf2')
