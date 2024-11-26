8759f428c9244bd06388d9b3dee446bf216161ba
Walburr <valentinhickel0@gmail.com>
2024-11-26 23:39:05 +0100

    JSP
    I had stars status mod i need to finish the css about it and some things in the html and after that i need to do the mecanic part about the filter like ascending order , status stars ....

--- Modsell/modsell.js
+++ Modsell/modsell.js
@@ -1,16 +1,18 @@
+
 var filterbox = document.getElementById('filter');
 var filtermenu = document.getElementById('filter-menu-pos');
+var optionfilter_as_de_ord = document.querySelector('.styled-select')
+var option_value_asds = [ "option1" , "option2"]
+var disabled_option = document.getElementById("disable_option")
 
 function adjustFontSize() {
     const screenWidth = window.innerWidth;
     const fontSize = screenWidth / 50;
 
-    // Sélectionne l'élément avec une classe ou un id valide
     var filtertxtttl = document.querySelector('.filtertxtttl');
     if (filtertxtttl) {
         filtertxtttl.style.fontSize = fontSize + 'px';
 
-        // Si la taille de la police est inférieure à 14px, cache l'élément
         if (fontSize < 14) {
             filtertxtttl.style.display = "none";
         } else {
@@ -19,22 +21,68 @@ function adjustFontSize() {
     }
 }
 
-// Écouteurs d'événements
 window.addEventListener("load", function () {
     adjustFontSize();
     window.addEventListener('resize', adjustFontSize);
+
+
+    filterbox.addEventListener('click', function () {
+        if (filtermenu.classList.contains('active')) {
+            filtermenu.classList.remove('active');
+            setTimeout(() => {
+                filtermenu.style.display = 'none';
+            }, 500);
+        } else {
+            filtermenu.style.display = 'flex';
+            setTimeout(() => {
+                filtermenu.classList.add('active');
+            }, 10);
+        }
+    });
+
+
+
+    optionfilter_as_de_ord.addEventListener('change' , function () {
+
+        
+
+        switch(optionfilter_as_de_ord.value) {
+            case "option1":
+                disabled_option.style.display = "none"; optionfilter_as_de_ord.style.color = "#658C9C"
+                optionfilter_as_de_ord.textcontent = option_value_asds.value
+                break;
+            case 'option2':
+                disabled_option.style.display = "none"; optionfilter_as_de_ord.style.color = "#658C9C"
+                optionfilter_as_de_ord.textcontent = option_value_asds.value 
+                break;
+            default :
+                optionfilter_as_de_ord.textContent = "Choose an option"; optionfilter_as_de_ord.style.color = 'red'
+        }
+    })
+
+
+    const stars = document.querySelectorAll('#star');
+
+    stars.forEach((star, index) => {
+        
+      star.addEventListener('mouseenter', () => {
+        
+        resetStars();
+
+        for (let i = 0; i <= index; i++) {
+          stars[i].classList.add('gold');
+          stars[i].style.fill = 'gold'; 
+        }
+      });
+    });
+
+    function resetStars() {
+        stars.forEach(star => {
+            star.classList.remove('gold');
+            star.style.fill = "none"
+        });
+    }
+
+
 });
 
-filterbox.addEventListener('click', function () {
-    if (filtermenu.classList.contains('active')) {
-        filtermenu.classList.remove('active');
-        setTimeout(() => {
-            filtermenu.style.display = 'none';
-        }, 500); // Attendez que la transition soit terminée
-    } else {
-        filtermenu.style.display = 'flex';
-        setTimeout(() => {
-            filtermenu.classList.add('active');
-        }, 10);
-    }
-});
